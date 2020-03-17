5a7d3e88cb9d78a1dd6c1fe939d0abc8c625337a
Alex <dok-chernikov@mail.ru>
2020-03-17 15:55:37 +0700

    add program geometry.

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+
 int main()
 {
-    printf("hello world\n");
+    FILE* file;
+    struct figure {
+        char name[10];
+        int p[3];
+    };
+    struct figure circle[10];
+    int i = 0;
+    file = fopen("circle.txt", "r");
+
+    while (fscanf(file,
+                  "%s (%d %d, %d)",
+                  circle[i].name,
+                  &circle[i].p[0],
+                  &circle[i].p[1],
+                  &circle[i].p[2])
+           != EOF) {
+        printf("%s (%d %d, %d)\n",
+               circle[i].name,
+               circle[i].p[0],
+               circle[i].p[1],
+               circle[i].p[2]);
+        i++;
+    }
     return 0;
 }
